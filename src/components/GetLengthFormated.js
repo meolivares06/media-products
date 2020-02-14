56d8fa0d9b9f8e7bc353ab43b7d2702dfb6388f1
Mario Olivares <mario.olivares@datys.cu>
2020-02-14 08:12:03 -0500

    Remove debugger

--- src/components/GetLengthFormated.js
+++ src/components/GetLengthFormated.js
@@ -1,13 +1,6 @@
 const GetLengthFormated = seconds => {
   try{
 
-    /*if (seconds < 60) return seconds;
-    if (seconds === 60) return 1;
-    else if (seconds === 61) return "01:01";
-    else if (seconds > 60) {
-      let minutos = seconds / 60;
-      if (minutos < 60) return `${minutos}:resto`;
-    }*/
     if(undefined === seconds || null === seconds || '' === seconds)return '';
     seconds = parseInt(seconds);
     let hour = Math.floor(seconds / 3600);
@@ -18,7 +11,6 @@ const GetLengthFormated = seconds => {
     second = second < 10 ? "0" + second : second;
 
     if (seconds < 60) return second;
-    debugger;
     if (seconds === 60) return "01:00";
     if (seconds > 60 && seconds < 3600) return `${minute}:${second}`;
     return hour + ":" + minute + ":" + second;
